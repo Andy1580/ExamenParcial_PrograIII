4de6acbb31948b364d52d3347b17fd570034144e
Andy1580 <andreslopezch1996@gmail.com>
2023-06-24 18:39:12 -0600

    Git creado

---
+++ Assets/Scripts/FxControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxControl : MonoBehaviour
+{
+    Rigidbody physics;
+    
+    [SerializeField] private float vel;
+
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+        physics.velocity = transform.forward*vel;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
